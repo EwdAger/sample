f94255ee2cd208af33bf794c0d25ddd0227a0cec
EwdAger <ewdager@hotmail.com>
2018-03-15 10:56:11 +0800

    Add password resets & email configuration

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,11 @@
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
 @include('layouts._header')
 
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -3,11 +3,20 @@
     <a href="{{ route('users.show', $user->id )}}" class="username">{{ $user->name }}</a>
 
     @can('destroy', $user)
-        <form action="{{ route('users.destroy', $user->id) }}" method="post">
+        <form action="{{ route('users.destroy', $user->id) }}" method="post" onsubmit="return sumbit_sure()">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
             <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
         </form>
     @endcan
 </li>
-
+<script language="javascript">
+    function sumbit_sure(){
+        var gnl=confirm("确定要提交?");
+        if (gnl==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    </script>
